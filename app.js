f9ff12637c59bf006c1e325bdcd84ed79b90c0b3
MoriHp <moriko232@gmail.com>
2022-05-02 16:52:31 +0800

    fix bug

--- app.js
+++ app.js
@@ -2,15 +2,16 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
-var cors = require("cors");
+// var cors = require("cors");
 
-var cors = require("cors");
+// var cors = require("cors");
+require("./connect/db.js");
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 
 var app = express();
 
-app.use(cors());
+// app.use(cors());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
